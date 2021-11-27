ed37a512994906165112067432961ea5ca46cd80
Elena Atanasova <ea01100@surrey.ac.uk>
2021-11-27 12:58:20 +0000

    Added testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'Art.ea.istic'
+    assert_select 'h1', 'Welcome to Art.ea.istic! :)'
+    assert_select 'p', 'Here you can keep track of interesting pieces of art and draw inspiration from the work of famous artists.'
   end
 
 end

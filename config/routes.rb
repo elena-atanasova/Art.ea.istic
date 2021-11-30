bea80df6965b53018c130cd8621f98d4a9873546
Elena Atanasova <ea01100@surrey.ac.uk>
2021-11-30 21:44:56 +0000

    webform and change to haml files

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'home#home'
   resources :artworks
   root 'artworks#index'
+  get 'webform', to: 'home#webform'
+  get 'addartwork', to: 'home#webform'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

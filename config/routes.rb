24e7e2ab301824d60b5803697315d36a57992f12
Elena Atanasova <ea01100@surrey.ac.uk>
2021-12-02 00:26:31 +0000

    style changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root 'home#home'
   resources :artworks
-  root 'artworks#index'
 
   get 'addartwork', to: 'home#webform'
   get 'contact', to: 'home#contact'
 
   post 'request_contact', to: 'home#request_contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
